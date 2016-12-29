1ab57672aa73e5e7a2fb2275523256bd8b5c7237
angelkon <angelkon@geninetworks.com>
2016-12-30 00:35:17 +0900

    package 수정 ssh2.js 파일 추가

---
+++ NodeJsApplication/ssh2.js
@@ -0,0 +1,39 @@
+var Client = require('ssh2').Client;
+
+var conn = new Client();
+
+var hostname = '';
+var username = '';
+var password = '';
+
+if (process.argv.length === 5){
+    hostname = process.argv[2];
+    username = process.argv[3];
+    password = process.argv[4];
+} else {
+    throw new Error("not enough arguments");
+}
+
+
+conn.on('ready', function () {
+    console.log('Client :: ready');
+    conn.shell(function (err, stream) {
+        if (err)
+            throw err;
+        stream.on('close', function () {
+            console.log('Stream :: close');
+            conn.end();
+        }).on('data', function (data) {
+            console.log('STDOUT:\n' + data);
+        }).stderr.on('data', function (data) {
+            console.log('STDERR:\n' + data);
+        });
+        stream.end('ls -l\nexit\n');
+    });
+}).connect({
+    host: hostname,
+    port: 10022,
+    username: username,
+    password: password
+});
+ 
